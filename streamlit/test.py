1ee70dde8c1ed6cf35536edcee3c5bde8baac01a
minguj <minguj.jin@gmail.com>
2025-01-21 10:20:02 +0900

    test

--- streamlit/test.py
+++ streamlit/test.py
@@ -4,6 +4,9 @@ import os
 import jpype
 import subprocess
 
+
+subprocess.run(['sudo', 'apt-get', 'install', '-y', 'openjdk-11-jdk'], check=True)
+
 # 패키지 설치 확인 (openjdk-11-jdk가 설치되었는지 확인)
 result = subprocess.run(["dpkg-query", "-l", "openjdk-11-jdk"], capture_output=True, text=True)
 
