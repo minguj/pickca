76cba3ee9b1fcfa2b758ba6d4c3f094a73d37b26
minguj <minguj.jin@gmail.com>
2025-01-21 09:58:26 +0900

    test

--- streamlit/test.py
+++ streamlit/test.py
@@ -1,5 +1,15 @@
 import streamlit as st
 from konlpy.tag import Okt
+import os
+import jpype
+
+# JAVA_HOME 환경 변수 설정
+os.environ['JAVA_HOME'] = '/usr/lib/jvm/java-11-openjdk-amd64'
+os.environ['PATH'] += os.pathsep + os.path.join(os.environ['JAVA_HOME'], 'bin')
+
+# JVM 시작 (필요 시 수동으로 시작)
+if not jpype.isJVMStarted():
+    jpype.startJVM(jpype.getDefaultJVMPath())
 
 st.title("Streamlit + konlpy 테스트")
 
