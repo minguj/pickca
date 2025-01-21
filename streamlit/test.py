82a975a0826d5055ca59cb8133e85b43a725f5b2
minguj <minguj.jin@gmail.com>
2025-01-21 10:25:15 +0900

    test

--- streamlit/test.py
+++ streamlit/test.py
@@ -15,15 +15,15 @@ else:
 
 
 
-# st.title("Streamlit + konlpy 테스트")
+st.title("Streamlit + konlpy 테스트")
 
-# # 텍스트 입력
-# text = st.text_input("텍스트를 입력하세요:")
-# if text:
-#     try:
-#         okt = Okt()
-#         tokens = okt.morphs(text)
-#         st.write("형태소 분석 결과:")
-#         st.write(tokens)
-#     except Exception as e:
-#         st.error(f"에러 발생: {e}")
+# 텍스트 입력
+text = st.text_input("텍스트를 입력하세요:")
+if text:
+    try:
+        okt = Okt()
+        tokens = okt.morphs(text)
+        st.write("형태소 분석 결과:")
+        st.write(tokens)
+    except Exception as e:
+        st.error(f"에러 발생: {e}")
